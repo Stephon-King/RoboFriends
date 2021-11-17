524561e3a8db5b0b00260836fe6abe0a36456f69
steffun <steffun@gmail.com>
2021-11-17 02:40:27 -0400

    Updates

---
+++ precache-manifest.5b1d52e7fcf4e94bcd2042391a8055ed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67090ddc9fe0d36f2bdb8f4d1f7baba2",
+    "url": "/RoboFriendsHooks/index.html"
+  },
+  {
+    "revision": "f8c685173aec53642a38",
+    "url": "/RoboFriendsHooks/static/css/2.bb824cd3.chunk.css"
+  },
+  {
+    "revision": "70439332dcfdb7925352",
+    "url": "/RoboFriendsHooks/static/css/main.afaef127.chunk.css"
+  },
+  {
+    "revision": "f8c685173aec53642a38",
+    "url": "/RoboFriendsHooks/static/js/2.29b14aae.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/RoboFriendsHooks/static/js/2.29b14aae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f3d753461fa6c0bc0a",
+    "url": "/RoboFriendsHooks/static/js/3.32e500d0.chunk.js"
+  },
+  {
+    "revision": "70439332dcfdb7925352",
+    "url": "/RoboFriendsHooks/static/js/main.8738c73c.chunk.js"
+  },
+  {
+    "revision": "cc4fea7f931019310a6e",
+    "url": "/RoboFriendsHooks/static/js/runtime-main.4495cc05.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriendsHooks/static/media/SEGA.12a3db8b.woff"
+  }
+]);
